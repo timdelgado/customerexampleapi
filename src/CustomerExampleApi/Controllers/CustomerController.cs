a1efe0429acf0747aac07f5f9e2f6c63b85b57ea
Tim <timdelgado@gamestop.com>
2016-08-10 14:31:08 -0500

    Initial commit

---
+++ src/CustomerExampleApi/Controllers/CustomerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using CustomerExampleApi.DataObjects;
+using System.Linq;
+
+namespace CustomerExampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        private IEnumerable<Customer> _customers;
+
+        public CustomerController()
+        {
+            var customers = new List<Customer>();
+            customers.Add(new Customer(1, new DateTime(1940, 01, 05), "Tim", "Delgado"));
+            customers.Add(new Customer(2, new DateTime(1950, 02, 10), "Shane", "Gibbons"));
+            customers.Add(new Customer(3, new DateTime(1960, 03, 15), "Mike", "Pennington"));
+            customers.Add(new Customer(4, new DateTime(1970, 04, 20), "John", "Boldt"));
+            customers.Add(new Customer(4, new DateTime(1980, 05, 25), "Divyash", "Patel"));
+            _customers = customers;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return _customers;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Customer Get(int id)
+        {
+            return _customers.Where(x => (x.Id == id)).Single();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/CustomerExampleApi/DataObjects/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerExampleApi.DataObjects
+{
+    public class Customer
+    {
+        public Customer(int id, DateTime dateOfBirth, string firstName, string lastName)
+        {
+            this.Id = id;
+            this.DateOfBirth = dateOfBirth;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public int Id { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        
+        public int Age
+        {
+            get { return ((DateTime.Now - DateOfBirth).Days)/365 ; }
+        }
+    }
+}
